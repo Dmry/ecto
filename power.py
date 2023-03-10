f49a8ed46a9ccc41adaf4efcf18bcd3c9a2d4861
Daniël Emmery <d.p.emmery@tue.nl>
2023-03-10 10:47:02 +0100

    Cleanup

--- power.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from utility import pairwise
-from input import Signal
-
-def calc_power(chan_a : Signal, chan_c : Signal, time, envelope_time):
-    i = (np.abs(time - envelope_time)).argmin()
-
-    return chan_a[0:i] * chan_c[0:i]
--- utility.py
+++
@@ -1,7 +0,0 @@
-def pairwise(iterable):
-    it = iter(iterable)
-    a = next(it, None)
-
-    for b in it:
-        yield (a, b)
-        a = b
